405ed9766bf2d9fa29a2f6a0063b122f9908493a
kamalkatyal <kamal1.katyal@infogain.com>
2026-08-19 16:07:44 -0400

    Changes wrt router post

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const router = express.Router();
-router.get('/', (request, response)=> response.send('hello from skillsoft1'));
+router.get('/', (request, response)=> response.send('hello from skillsoft...'));
+router.post('/addemployee', (request, response) => {
+let empName = request.body.empName;
+let empPass = request.body.empPass;
+response.end(`POST success, you sent ${empName} and ${empPass},
+thanks!`);
+});
 app.use('/', router);
 app.listen(port, function(){
 console.log("Listening " + port);
