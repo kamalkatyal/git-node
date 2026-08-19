a2dcc03bddcfd8984a1a3bea2ac66306f53f49a5
kamalkatyal <kamal1.katyal@infogain.com>
2026-08-19 17:33:49 -0400

    Completed index files to work with routes

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
+const routes = require('./routes/routes');
 const port = 8000;
 const app = express();
 const router = express.Router();
+routes(router);
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-router.get('/', (request, response)=> response.send('hello from skillsoft...'));
-router.post('/addemployee', (request, response) => {
-let empName = request.body.empName;
-let empPass = request.body.empPass;
-response.end(`POST success, you sent ${empName} and ${empPass},
-thanks!`);
-});
 app.use('/', router);
 app.listen(port, function(){
 console.log("Listening " + port);
