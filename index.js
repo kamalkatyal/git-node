2f3b28f60d312ccac0c6193b26412fcdd2771195
kamalkatyal <kamal1.katyal@infogain.com>
2026-08-19 16:15:41 -0400

    Changes wrt router post json

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 router.get('/', (request, response)=> response.send('hello from skillsoft...'));
 router.post('/addemployee', (request, response) => {
 let empName = request.body.empName;
